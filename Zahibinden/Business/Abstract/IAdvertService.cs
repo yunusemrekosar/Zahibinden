dc2a123981143efe3b7afda7746e98f56dbe80d6
Sahil Amirov <69482519+SahilEmirov@users.noreply.github.com>
2023-06-02 01:57:51 +0300

    crud

--- Zahibinden/Business/Abstract/IAdvertService.cs
+++ Zahibinden/Business/Abstract/IAdvertService.cs
@@ -8,8 +8,15 @@ namespace Zahibinden.Business.Abstract
         bool AddAdvert(VM_CreateAdvert advert);
         bool UpdateAdvert(VM_UpdateAdvert advert);
         bool DeleteAdvert(int advertId);
+
         Advert GetAdvertById(int advertId);
         List<Advert> GetAllAdverts();
         List<Advert> GetAdvertsByUserId(int userId);
+
+        VM_ShowAdvert GetAdvertByIdShow(int advertId);
+        List<VM_ShowAdvert> GetAllAdvertsShow();
+        List<VM_ShowAdvert> GetAdvertsByUserIdShow(int userId);
+
+        VM_UpdateAdvert GetAdvertByIdUpdate(int advertId);
     }
 }
--- Zahibinden/Models/ViewModels/VM_UpdateAdvert.cs
+++ Zahibinden/Models/ViewModels/VM_UpdateAdvert.cs
@@ -5,10 +5,10 @@ namespace Zahibinden.Models.ViewModels
     public class VM_UpdateAdvert
     {
         public string Title { get; set; }
-        public Guid CategoryId { get; set; }
-        public Guid UserId { get; set; }
-        public Guid CityId { get; set; }
-        public Guid TypeId { get; set; }
+        public int CategoryId { get; set; }
+        //public int UserId { get; set; }
+        public int CityId { get; set; }
+        public int TypeId { get; set; }
         public double Price { get; set; }
         public string NumberofRooms { get; set; }
         public string? Description { get; set; }
